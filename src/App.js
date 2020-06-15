0c723a532772b3d7b84c95587b7d67cc6e32f9d9
vladcimpianu <vladcimpianu@gmail.com>
2020-06-15 17:27:49 +0300

    added modal

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import Range from './components/Range';
+import Modal from './components/Modal';
 import './components/range.css';
+import './components/modal.css';
 import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      rangeVal: 0
+      rangeVal: 0,
+      isOpen: false
     }
     this.updateRange = this.updateRange.bind(this);
   }
@@ -21,8 +24,18 @@ class App extends React.Component {
   render() {
     const { rangeVal } = this.state;
     return (
-      <div>
+      <div className='App'>
         <Range range={rangeVal} updateRange={this.updateRange}/>
+        <button 
+          className='modal-btn' 
+          onClick={ (e) => this.setState({ isOpen: true })}>
+        Open Modal
+        </button>
+        <Modal 
+          isOpen={this.state.isOpen} 
+          onClose={ (e) => this.setState({ isOpen: false })}
+        >Lorem ipsum id semper risus in hendrerit gravida rutrum quisque non tellus orci ac auctor augue mauris augue neque gravida in fermentum et sollicitudin ac orci phasellus egestas tellus rutrum tellus pellentesque
+        </Modal>  
       </div>
     )
   }  
